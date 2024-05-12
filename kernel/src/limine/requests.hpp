865ba157d4fcd27bb7b35d3e0e54739f97c051d6
ka <kaku.h9@gmail.com>
2024-05-12 14:47:01 +0100

    Grouped limine functionalities together

--- kernel/src/limine/requests.hpp
+++ kernel/src/limine/requests.hpp
@@ -1,13 +1,15 @@
-#include "limine_services.hpp"
+#ifndef SERVICES_HPP
+#define SERVICES_HPP
+
 #include "limine.h"
 
-namespace req{
+namespace limine::requests{
 
 // Set the base revision to 1, this is recommended as this is the latest
 // base revision described by the Limine boot protocol specification.
 // See specification for further info
 
-bool is_limine_supported(){
+inline bool is_limine_supported(){
   // checks to see if the bootloader supports our standard
   LIMINE_BASE_REVISION(1);
   if(LIMINE_BASE_REVISION_SUPPORTED == false){
@@ -22,28 +24,14 @@ bool is_limine_supported(){
 // be made volatile or equivalent, _and_ they should be accessed at least
 // once.
 
-volatile limine_framebuffer_request framebuffer_request = {
-  .id = LIMINE_FRAMEBUFFER_REQUEST,
-  .revision = 0,
-  .response = nullptr
-};
+extern volatile limine_framebuffer_request framebuffer_request;
 
-volatile limine_memmap_request memorymap_request{
-  .id = LIMINE_MEMMAP_REQUEST,
-  .revision = 0,
-  .response = nullptr
-};
+extern volatile limine_memmap_request memorymap_request;
 
-volatile limine_hhdm_request hhdm_request{
-  .id = LIMINE_HHDM_REQUEST,
-  .revision = 0,
-  .response = nullptr
-};
+extern volatile limine_hhdm_request hhdm_request;
 
-volatile limine_kernel_address_request kernel_addr_req{
-  .id = LIMINE_KERNEL_ADDRESS_REQUEST,
-  .revision = 0,
-  .response = nullptr
-};
+extern volatile limine_kernel_address_request kernel_addr_req;
 
-} // namespace req
+} // namespace limine::requests
+
+#endif
--- kernel/src/memory/memory.hpp
+++ kernel/src/memory/memory.hpp
@@ -1,27 +1,27 @@
 #ifndef MEMORY_HPP
 #define MEMORY_HPP
 #include <cstdint>
-#include <limine.h>
-#include <limine_services.hpp>
 #include <kostream.hpp>
+#include "limine/limine.h"
+#include "limine/requests.hpp"
 
-namespace req{
+namespace limine::requests{
 extern volatile limine_hhdm_request hhdm_request;
 extern volatile limine_memmap_request memorymap_request;
 extern volatile limine_kernel_address_request kernel_addr_req;
 }
 
-namespace mem{
+namespace memory{
   // a bunch of miscellenaeous memory
   using paddr64_t = std::uint64_t;
   using vaddr64_t = std::uint64_t;
 
   inline paddr64_t vaddrToPaddr(vaddr64_t vaddr){
-    return vaddr - req::hhdm_request.response->offset;
+    return vaddr - limine::requests::hhdm_request.response->offset;
   }
 
   inline vaddr64_t paddrToVaddr(paddr64_t paddr){
-    return paddr + req::hhdm_request.response->offset;
+    return paddr + limine::requests::hhdm_request.response->offset;
   }
 
   void printMemoryMap();
